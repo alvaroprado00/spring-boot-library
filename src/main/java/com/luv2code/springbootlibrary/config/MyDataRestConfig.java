9c32ed20cebfd33527f07e354f5bba58906c3bec
alvaroprado00 <72304152+alvaroprado00@users.noreply.github.com>
2023-10-02 10:47:09 +0200

    Authentication
    This commit includes:
    
    - Checkout functionality
    - Book Service with custom functions
    - Book Controller to handle custom endpoints
    - Authentication with Okta
    - JWT email extraction

--- src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -1,5 +1,6 @@
 package com.luv2code.springbootlibrary.config;
 import com.luv2code.springbootlibrary.entity.Book;
+import com.luv2code.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -26,7 +27,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.POST};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /*Configure CORS Mapping*/
 
---
+++ src/main/java/com/luv2code/springbootlibrary/dao/CheckoutRepository.java
@@ -0,0 +1,14 @@
+package com.luv2code.springbootlibrary.dao;
+
+import com.luv2code.springbootlibrary.entity.Checkout;
+import org.hibernate.annotations.Check;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    Checkout findByUserEmailAndBookId(String userEmail, long bookId);
+
+    Collection<Checkout> findAllByUserEmail(String userEmail);
+}
